0c15a6088c344ddc823f4e86e5053698cb3ce552
Henry <th.saravana@gmail.com>
2018-10-11 19:22:20 +0530

    Documenting

--- src/main/kotlin/com/madrapps/handlebars/HbElementResolver.kt
+++ src/main/kotlin/com/madrapps/handlebars/HbElementResolver.kt
@@ -25,34 +25,6 @@ class HbElementResolver(private val templateClass: PsiClass) {
         return classes.findInDepth(element.findAncestorOfType<HbPath>(), position)
     }
 
-    private fun MutableList<PsiClass?>.findInDepth(fieldName: String): PsiField? {
-        reversed().forEach { psiClass ->
-            if (psiClass != null) {
-                val field = psiClass.findFieldByName(fieldName, true)
-                if (field != null) return field
-            }
-        }
-        return null
-    }
-
-    private fun MutableList<PsiClass?>.findInDepth(path: HbPath?, position: Int = 999): PsiField? {
-        if (path == null || position < 0) return null
-        val pathElements = path.children.filter { it.node.elementType == HbTokenTypes.ID }
-
-        var currentType: PsiField? = null
-        for (i in 0..position) {
-            currentType = if (i > pathElements.lastIndex) {
-                break
-            } else if (i == 0) {
-                findInDepth(pathElements[i].text)
-            } else {
-                (currentType?.type as? PsiClassReferenceType)?.resolve()?.findFieldByName(pathElements[i].text, true)
-            }
-        }
-        return currentType
-    }
-
-
     private fun findElement(hbElement: HbPsiElement): MutableList<PsiClass?> {
         val blockWrapper: HbBlockWrapper? = hbElement.findAncestorOfType()
         if (blockWrapper != null) {
@@ -83,6 +55,45 @@ class HbElementResolver(private val templateClass: PsiClass) {
             return mutableListOf(templateClass)
         }
     }
+
+    /**
+     * Find the PsiField for a given HbPsiElement. The HbPsiElement is not directly supplied to this method, but rather
+     * via 2 parameters (the parent HbParam and the position of HbPsiElement in the parent). For the first HbPsiElement,
+     * we will search in-depth (since that's how scope works in handlebars java) works. But for successive HbPsiElement,
+     * we will only search for fields of the PsiClass of the previous HbPsiElement.
+     *
+     * @param path the HbPath element which can have multiple children, but we are concerned only the ID type
+     * @param position the position of the child we want to resolve in the HbPath element. For instance, if the HbPath
+     * element is `zero.one.two.three`, and the HbPsiElement to resolve is `two`, then the position will be 2. Note that,
+     * HbPath has 7 children, but only 4 of them are of Type ID. Hence the position is 2. The position field starts with 0.
+     */
+    private fun MutableList<PsiClass?>.findInDepth(path: HbPath?, position: Int = 999): PsiField? {
+        if (path == null || position < 0) return null
+        val pathElements = path.children.filter { it.node.elementType == HbTokenTypes.ID }
+
+        var currentType: PsiField? = null
+        for (i in 0..position) {
+            currentType = if (i > pathElements.lastIndex) {
+                break
+            } else if (i == 0) {
+                findInDepth(pathElements[i].text)
+            } else {
+                (currentType?.type as? PsiClassReferenceType)?.resolve()?.findFieldByName(pathElements[i].text, true)
+            }
+        }
+        return currentType
+    }
+
+    private fun MutableList<PsiClass?>.findInDepth(fieldName: String): PsiField? {
+        reversed().forEach { psiClass ->
+            if (psiClass != null) {
+                val field = psiClass.findFieldByName(fieldName, true)
+                if (field != null) return field
+            }
+        }
+        return null
+    }
+
 }
 
 private fun <E> MutableList<E>.addAndReturn(element: E): MutableList<E> {
--- src/main/kotlin/com/madrapps/handlebars/Plugin.kt
+++ src/main/kotlin/com/madrapps/handlebars/Plugin.kt
@@ -30,10 +30,19 @@ internal fun HbParam.findHbPath(): HbPath? {
     return null
 }
 
+/**
+ * Check if the element is a block parameter like {{#each parameter}} or {{#with parameter}}. If `parameter` is the element,
+ * then this method with return `true`
+ */
 internal fun HbPsiElement.isBlockParameter(): Boolean {
     return parent?.parent?.parent is HbParam
 }
 
+/**
+ * Find the position of the element in the parent. A parent can have many children, but this method returns the position
+ * relative to the IElementType of the children. So if the type is ID, then only those children are considered, and the position
+ * is returned based on that.
+ */
 internal fun HbPsiElement.childPositionInParent(type: IElementType): Int {
     val filteredChildren = parent.children.filter { it.node.elementType == type }
     return filteredChildren.indexOf(this)
