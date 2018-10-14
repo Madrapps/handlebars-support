9106900215685c9b2db569778fe5b2d52f5cb7d3
Henry <th.saravana@gmail.com>
2018-10-14 21:46:26 +0530

    `this` and `..` will work now at top level. Previously it worked only inside a Block wrapper (#2)

--- src/main/kotlin/com/madrapps/handlebars/HbElementResolver.kt
+++ src/main/kotlin/com/madrapps/handlebars/HbElementResolver.kt
@@ -23,9 +23,9 @@ class HbElementResolver(private val templateClass: PsiClass) {
 
         val findInDepth1 = classes.findInDepth1(element)
 
-        println("FOME -> ${findInDepth1?.name}")
+        println("FOME -> ${findInDepth1?.element?.text}")
 
-        return classes.findInDepth1(element)
+        return classes.findInDepth1(element)?.element
     }
 
     private fun findElement(hbElement: HbPsiElement?): MutableList<PsiGroup?> {
@@ -42,7 +42,7 @@ class HbElementResolver(private val templateClass: PsiClass) {
             when (hbMustacheName.name) {
                 // TODO "with" behaves same as "each", but we should ensure that Lists are not possible in "with" (inspection/autocompletion)
                 "each", "with" -> {
-                    val psiField = elementList.findInDepth1(hbParam.findHbPath())
+                    val psiField = elementList.findInDepth1(hbParam.findHbPath())?.psiField
                             ?: return elementList.addAndReturn(null)
                     val type = psiField.type as? PsiClassReferenceType
                     val psiClass = type?.resolveToClass() ?: return elementList.addAndReturn(null)
@@ -66,49 +66,49 @@ class HbElementResolver(private val templateClass: PsiClass) {
      * element is `zero.one.two.three`, and the HbPsiElement to resolve is `two`, then the position will be 2. Note that,
      * HbPath has 7 children, but only 4 of them are of Type ID. Hence the position is 2. The position field starts with 0.
      */
-    private fun MutableList<PsiGroup?>.findInDepth(path: HbPath?, position: Int = 999): PsiField? {
-        if (path == null || position < 0) return null
-        val segments = path.children.filter { it.node.elementType == ID }
+//    private fun MutableList<PsiGroup?>.findInDepth(path: HbPath?, position: Int = 999): PsiField? {
+//        if (path == null || position < 0) return null
+//        val segments = path.children.filter { it.node.elementType == ID }
+//
+//        var psiField: PsiField? = null
+//        var shouldDeepFind = false
+//        loop@ for (i in 0..position) {
+//            when (i) {
+//                segments.size -> (break@loop)
+//                0 -> when (segments[i].text) {
+//                    "this" -> psiField = last()?.psiField
+//                    ".." -> {
+//                        shouldDeepFind = true
+//                        psiField = dropLast(1).lastOrNull()?.psiField
+//                    }
+//                    else -> psiField = findInDepth(segments[i].text)
+//                }
+//                else -> {
+//                    when (segments[i].text) {
+//                        "this" -> {
+//                            shouldDeepFind = false
+//                            //psiField = psiField
+//                        }
+//                        ".." -> {
+//                            shouldDeepFind = true
+//                            psiField = dropLast(position + 1).lastOrNull()?.psiField
+//                        }
+//                        else -> {
+//                            if (shouldDeepFind) {
+//                                shouldDeepFind = false
+//                                psiField = dropLast(position).findInDepth(segments[i].text)
+//                            } else {
+//                                psiField = (psiField?.type as? PsiClassReferenceType)?.resolveToClass()?.findFieldByName(segments[i].text, true)
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        return psiField
+//    }
 
-        var psiField: PsiField? = null
-        var shouldDeepFind = false
-        loop@ for (i in 0..position) {
-            when (i) {
-                segments.size -> (break@loop)
-                0 -> when (segments[i].text) {
-                    "this" -> psiField = last()?.psiField
-                    ".." -> {
-                        shouldDeepFind = true
-                        psiField = dropLast(1).lastOrNull()?.psiField
-                    }
-                    else -> psiField = findInDepth(segments[i].text)
-                }
-                else -> {
-                    when (segments[i].text) {
-                        "this" -> {
-                            shouldDeepFind = false
-                            //psiField = psiField
-                        }
-                        ".." -> {
-                            shouldDeepFind = true
-                            psiField = dropLast(position + 1).lastOrNull()?.psiField
-                        }
-                        else -> {
-                            if (shouldDeepFind) {
-                                shouldDeepFind = false
-                                psiField = dropLast(position).findInDepth(segments[i].text)
-                            } else {
-                                psiField = (psiField?.type as? PsiClassReferenceType)?.resolveToClass()?.findFieldByName(segments[i].text, true)
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return psiField
-    }
-
-    private fun List<PsiGroup?>.findInDepth1(element: PsiElement?): PsiField? {
+    private fun List<PsiGroup?>.findInDepth1(element: PsiElement?): PsiGroup? {
         if (element == null) return null
         fun something(psiElement: PsiElement): List<PsiGroup?> {
             val previousSibling = psiElement.previousSiblingOfType(ID)
@@ -118,7 +118,7 @@ class HbElementResolver(private val templateClass: PsiClass) {
                     ".." -> return classes.dropLast(1)
                     "this" -> return classes.takeLast(1)
                     else -> {
-                        val psiField = classes.findInDepth(previousSibling.text)
+                        val psiField = classes.findInDepth(previousSibling.text)?.psiField
                         val psiClass = (psiField?.type as? PsiClassReferenceType)?.resolveToClass()
                         if (psiClass != null) {
                             return classes.toMutableList().addAndReturn(PsiGroup(psiField, psiClass))
@@ -138,18 +138,22 @@ class HbElementResolver(private val templateClass: PsiClass) {
             println("classes -> ${it?.psiField?.name} :: ${it?.psiClass?.name}")
         }
         return when (element.text) {
-            ".." -> something.dropLast(1).lastOrNull()?.psiField
-            "this" -> something.lastOrNull()?.psiField
+            ".." -> something.dropLast(1).lastOrNull()
+            "this" -> something.lastOrNull()
             else -> something.findInDepth(element.text)
         }
     }
 
-
-    private fun List<PsiGroup?>.findInDepth(fieldName: String): PsiField? {
+    private fun List<PsiGroup?>.findInDepth(fieldName: String): PsiGroup? {
         reversed().forEach { group ->
             if (group != null) {
                 val field = group.psiClass.findFieldByName(fieldName, true)
-                if (field != null) return field
+                if (field != null) {
+                    val psiClass = (field.type as? PsiClassReferenceType)?.resolveToClass()
+                    if (psiClass != null) {
+                        return PsiGroup(field, psiClass)
+                    }
+                }
             }
         }
         return null
