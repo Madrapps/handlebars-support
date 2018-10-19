f87f97060609b0cbee9e03d29de7cce73c446f9d
Henry <th.saravana@gmail.com>
2018-10-19 21:23:10 +0530

    Adding class chooser dialog (#12)

--- src/main/kotlin/com/madrapps/handlebars/notification/ConfigureClassEditorNotificationProvider.kt
+++ src/main/kotlin/com/madrapps/handlebars/notification/ConfigureClassEditorNotificationProvider.kt
@@ -1,9 +1,11 @@
 package com.madrapps.handlebars.notification
 
+import com.intellij.ide.util.TreeClassChooserFactory
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiClass
 import com.intellij.ui.EditorNotificationPanel
 import com.intellij.ui.EditorNotifications
 
@@ -26,12 +28,19 @@ class ConfigureClassEditorNotificationProvider(private val project: Project, pri
         val panel = EditorNotificationPanel()
         panel.setText("No class configured.")
         panel.createActionLabel("Configure") {
-            println("Configuring class")
+            val psiClass = chooseClassDialog("Choose class", project)
+            println("Class configured : " + psiClass.name)
         }
         panel.createActionLabel("Ignore") {
             isIgnored = true
             notifications.updateAllNotifications()
         }
         return panel
+    }
+
+    private fun chooseClassDialog(title: String, project: Project): PsiClass {
+        val dialog = TreeClassChooserFactory.getInstance(project).createAllProjectScopeChooser(title)
+        dialog.showDialog()
+        return dialog.selected
     }
 }
